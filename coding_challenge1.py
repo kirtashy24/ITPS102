69666fa166e978d218ea1a192e5a14d5349b8b6a
kirtashy24 <kirtroncale800@gmail.com>
2025-08-14 00:17:48 +0800

    Create coding_challenge1.py

---
+++ coding_challenge1.py
@@ -0,0 +1,3 @@
+name = input("Who are you")
+
+print("\t\t\t\t*\n\t\t\t*\t\t*\n\t\t*\t\t\t\t*\n\t*\t\t\tHi!\t\t\t*\n*\t\t\t\t", name,"\t\t\t*\n\t*\t\t\t\t\t\t*\n\t\t*\t\t\t\t*\n\t\t\t*\t\t*\n\t\t\t\t*")
