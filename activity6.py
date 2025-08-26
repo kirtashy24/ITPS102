4c31f03b0765a80684fb284db399b935854b4c31
kirtashy24 <kirtroncale800@gmail.com>
2025-08-26 08:02:42 +0800

    Create activity6.py

---
+++ activity6.py
@@ -0,0 +1,18 @@
+n1= eval(input("enter the first number:"))
+n2= eval (input("enter the second number:"))
+
+s = n1+n2
+d = n1-n2
+p = n1*n2
+q= n1/ n2
+e= n1**n2
+r= n1%n2
+f= n1//n2
+
+print("the sum of",n1, "and",n2,"is",s)
+print("the difference of",n1,"and",n2,"is",d)
+print("the product of",n1,"and",n2,"is",p)
+print("the quotient of",n1,"and",n2,"is",q)
+print(n1,"exponent by",n2, "is",e)
+print("the remainder of",n1,"and",n2,"is",r)
+print("the floor division of",n1,"and",n2, "is",f)
