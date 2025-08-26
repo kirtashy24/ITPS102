7ec111b2f081f0c57068f1e4fb7f3a42332e7adc
kirtashy24 <kirtroncale800@gmail.com>
2025-08-26 08:05:06 +0800

    Create activity9.py

---
+++ activity9.py
@@ -0,0 +1,14 @@
+a = 3
+b = 2
+c = 6
+d = 5 
+
+username='kiroash'
+password = 'kiroashy'
+
+print("Login Here")
+print((username == 'kiroash') and (password == 'kirtashywrong'))
+
+print((a > b) and (c < d))
+print((a < b) or (c < d))
+print(not((a < b) or (c < d)))
