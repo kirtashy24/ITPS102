f447e53a63fcebf0f0c5e000cdae8316244a8a6d
kirtashy24 <kirtroncale800@gmail.com>
2025-08-26 08:04:12 +0800

    Create activity7.py

---
+++ activity7.py
@@ -0,0 +1,13 @@
+x = 5
+                       
+print("the value of a is",x )
+
+x +=5
+
+print("the value of a is ",x )                            
+x *= 3
+x +=13
+x -= 4
+x *= 2
+
+print("the value of a is",x )
