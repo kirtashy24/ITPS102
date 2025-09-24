f9083b77dfe971a7f3b088ed9c18be24702970b8
kirtashy24 <kirtroncale800@gmail.com>
2025-09-24 12:34:47 +0800

    Create code_challenge10.py

---
+++ code_challenge10.py
@@ -0,0 +1,10 @@
+print("SPACESHIP COUNTDOWN TIMER")
+
+start_num = int(input("Enter the starting number for countdown: "))
+
+print("\nCountdown started:")
+
+for i in range(start_num, 0, -1):
+    print(i)
+
+print("Liftoff!")
